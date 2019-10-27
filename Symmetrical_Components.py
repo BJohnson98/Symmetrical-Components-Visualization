fafba46b330f38583bbb367b6e05def7d19c89f0
Brandon Johnson <47310376+BJohnson98@users.noreply.github.com>
2019-10-27 15:05:06 -0500

    Create Symmetrical_Components.py

---
+++ Symmetrical_Components.py
@@ -0,0 +1,79 @@
+'''
+This program will visualize the theorm of symmetrical components. You should be able to
+input a non balanced 3 phase system and this program will show the sequence components that
+the system is made out of.
+Author: Brandon Johnson
+Date: 10/27/2019
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+#quiver plots (start_x,start_y,end_x,end_y)
+def main():
+	A_radius = int(input("Enter Phase A's radius: "))
+	A_angle = int(input("Enter Phase A's angle: "))
+	B_radius = int(input("Enter Phase B's radius: "))
+	B_angle = int(input("Enter Phase B's angle: "))
+	C_radius = int(input("Enter Phase C's radius: "))
+	C_angle = int(input("Enter Phase C's angle: "))	
+	
+	plot_vector(A_radius,A_angle,'r')
+	plot_vector(B_radius,B_angle,'y')
+	plot_vector(C_radius,C_angle,'b')
+	plot(maximum(A_radius,B_radius,C_radius))
+
+
+
+def plot_vector(radius, angle, c):
+	x,y = pol2cart(radius,angle)
+	plt.quiver(0, 0, x, y, angles='xy', scale_units='xy', scale=1, color=c)
+
+def plot(max):
+	#plt.quiver([0, 0, 0], [0, 0, 0], [1, -2, 4], [1, 2, -7], angles='xy', scale_units='xy', scale=1, color=['r','y','b'])
+	#plt.quiver(0, 0, 1, 0, angles='xy', scale_units='xy', scale=1, color='g')
+	plt.xlim(-max*1.1, max*1.1)
+	plt.ylim(-max*1.1, max*1.1)
+	plt.grid()
+	plt.gca().set_aspect('equal', adjustable='box')
+	plt.show()
+	
+'''
+Converts cartesian coordinates to polar. 
+Output for angle is degrees
+Author: Brandon Johnson.
+Date created: 10/27/2019
+'''
+def cart2pol(x, y):
+	rho = np.sqrt(x**2 + y**2)
+	phi = np.arctan2(y, x)*180/math.pi
+	return(round(rho,2), round(phi,2))
+	
+'''
+Converts polar coordinates to cartesian. This function
+is used when plotting each phase. Angles are inputed in degrees
+Author: Brandon Johnson.
+Date created: 10/27/2019
+'''
+def pol2cart(rho, phi):
+	phi = phi*math.pi/180
+	x = rho * np.cos(phi)
+	y = rho * np.sin(phi)
+	if y<0.001 and y>-0.001:
+		y=0
+	if x<0.00001 and x>-0.001:
+		x=0
+	return(round(x,2),round(y,2))
+
+'''
+Finds max of 3 numbers. Used for scaling plot.
+Author: Brandon Johnson.
+Date created: 10/27/2019
+'''
+def maximum(a, b, c): 
+    list = [a, b, c] 
+    return max(list) 
+	
+if __name__ == "__main__":
+	main()
+
